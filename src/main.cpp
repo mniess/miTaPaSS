5763c1c36e664e9c7b42716a1d60413389084acc
Michael Nieß <randusr@gmx.de>
2017-08-02 17:01:25 +0200

    add more options for help argument
    allowed: "help","-h","--help"

--- src/main.cpp
+++ src/main.cpp
@@ -5,8 +5,8 @@
 int main(int argc,char* argv[]) {
 	std::string configFile = "default.cfg";
 	if(argc==2){
-		if( argv[1] == std::string("help") ){
-			std::cout << "Usage: " << argv[0] << " [CONF-FILE]" << std::endl;
+		if( argv[1] == std::string("help") || argv[1] == std::string("-h") || argv[1] == std::string("--help")){
+			std::cout << "Usage: " << argv[0] << " [CONF_FILE]" << std::endl;
 			return 0;
 		} else{
 			configFile = argv[1];
